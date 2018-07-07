f5f0554bc8c2ce492469898ba8cecb86f604b70f
FrancoisBotha <botha.francois@gmail.com>
2018-07-07 12:23:51 +0200

    Cleaned up S3 Service

--- src/main/java/io/francoisbotha/namazingserver/config/S3Config.java
+++ src/main/java/io/francoisbotha/namazingserver/config/S3Config.java
@@ -7,36 +7,32 @@ package io.francoisbotha.namazingserver.config;
  * amazon-s3-uploaddownload-files-springboot-amazon-s3-application
  ****************************/
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 @Configuration
 public class S3Config {
-    @Value("${aws.access_key_id}")
-    private String awsId;
 
-    @Value("${aws.secret_access_key}")
-    private String awsKey;
+    @Value("${aws.s3.profile}")
+    private String awsProfileName;
 
-    @Value("${jsa.s3.region}")
+    @Value("${aws.s3.region}")
     private String region;
 
     @Bean
-    public AmazonS3 s3client() {
+        public AmazonS3 s3client() {
 
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsId, awsKey);
-        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                .withRegion(Regions.fromName(region))
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-                .build();
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withCredentials(new ProfileCredentialsProvider(awsProfileName))
+                    .withRegion(Regions.fromName(region))
+                    .build();
 
-        return s3Client;
+            return s3Client;
     }
 }
