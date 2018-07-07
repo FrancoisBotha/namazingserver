231c8ea3cdb8ab4ed552e6f39a65e96dd780dd3d
FrancoisBotha <botha.francois@gmail.com>
2018-07-07 12:24:13 +0200

    DAO added

---
+++ src/main/java/io/francoisbotha/namazingserver/domain/dao/VendorRepository.java
@@ -0,0 +1,13 @@
+//package io.francoisbotha.namazingserver.domain.dao;
+//
+//import io.francoisbotha.namazingserver.domain.model.Vendor;
+//import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import java.util.List;
+//
+//@EnableScan
+//public interface VendorRepository extends CrudRepository<Vendor, String> {
+//
+//    List<Vendor> findById(String id);
+//}
